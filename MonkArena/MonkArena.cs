ba8057fe81153e3297da6fb1378d49b2985dc080
Gavin White <horace5413@gmail.com>
2020-03-02 10:20:43 -0800

    Versioning

--- MonkArena/MonkArena.cs
+++ MonkArena/MonkArena.cs
@@ -7,5 +7,15 @@ using System.Runtime.InteropServices;
 
 namespace MonkArena {
     public class MonkArena : Partiality.Modloader.PartialityMod {
+        public string MajorVersion { get; } = "0";
+        public string MinorVersion { get; } = "0";
+        public string Revision { get; } = "1";
+
+        public MonkArena() {
+            author = "Little Tiny Big";
+            ModID = "MonkArena";
+            Version = $"{MajorVersion}.{MinorVersion}.{Revision}";
+        }
+
     }
 }

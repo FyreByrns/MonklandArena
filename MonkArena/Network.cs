912c9cc994449d251759946ceb84c79a1b7f0df4
Gavin White <horace5413@gmail.com>
2020-03-02 10:52:56 -0800

    Networking ~ Udp

---
+++ MonkArena/Network.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MonkArena {
+    static class Network {
+    }
+
+    public struct Received {
+        public IPEndPoint Sender { get; set; }
+        public string Message { get; set; }
+    }
+
+    abstract class UdpBase {
+        protected UdpClient Client;
+
+        protected UdpBase() {
+            Client = new UdpClient();
+        }
+
+        public async Task<Received> Receive() {
+            var result = await Client.ReceiveAsync();
+            return new Received() {
+                Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                Sender = result.RemoteEndPoint,
+            };
+        }
+    }
+
+    class UdpListener : UdpBase {
+        IPEndPoint listenOn;
+
+        public UdpListener() : this(new IPEndPoint(IPAddress.Any, 19000)) { }
+        public UdpListener(IPEndPoint endpoint) {
+            listenOn = endpoint;
+            Client = new UdpClient(listenOn);
+        }
+
+        public void Reply(string message, IPEndPoint endpoint) {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length, endpoint);
+        }
+    }
+
+    class UdpUser : UdpBase {
+        private UdpUser() { }
+
+        public static UdpUser ConnectTo(string hostname, int port) {
+            var connection = new UdpUser();
+            connection.Client.Connect(hostname, port);
+            return connection;
+        }
+
+        public void Send(string message) {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length);
+        }
+    }
+}

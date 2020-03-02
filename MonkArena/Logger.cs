d5b6fe584db65867e791577a3a0e026b002c0ed2
Gavin White <horace5413@gmail.com>
2020-03-02 10:28:37 -0800

    Basic logging

--- MonkArena/Logger.cs
+++ MonkArena/Logger.cs
@@ -4,10 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
 
 namespace MonkArena {
     public static class Logger {
+        static StreamWriter output;
 
+        static Logger() {
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = Encoding.GetEncoding(437);
+            output = new StreamWriter(fileStream, encoding);
+            output.AutoFlush = true;
+        }
+
+        public static void Log(object message, string prefix = "INFO") {
+            output.WriteLine($"[{prefix}][{DateTime.UtcNow}] {message}");
+        }
+        public static void LogError(object message) {
+            Log(message, "ERROR");
+        }
+        public static void LogInfo(object message) {
+            Log(message, "INFO");
+        }
 
         #region Interop
         [DllImport(

6fe3de920bf897f891c4ee19b81d3e5440bff5e1
Gavin White <horace5413@gmail.com>
2020-03-02 10:24:08 -0800

    Logger interop

--- MonkArena/Logger.cs
+++ MonkArena/Logger.cs
@@ -7,5 +7,24 @@ using System.Runtime.InteropServices;
 
 namespace MonkArena {
     public static class Logger {
+
+
+        #region Interop
+        [DllImport(
+            "kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport(
+            "kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        #endregion
     }
 }

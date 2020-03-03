814ab45189994610c1ed0ac9fca95c2bd6143ef6
Gavin White <horace5413@gmail.com>
2020-03-03 13:15:16 -0800

    Message serialization to string and deserialization from string

--- MonkArena/Message.cs
+++ MonkArena/Message.cs
@@ -11,18 +11,26 @@ namespace MonkArena {
 
         public static string GetToken(string s) => s.Split(')')[1].Split(':')[0];
 
+        private Message() { }
+        public Message(string from) {
+            string[] data = from.Split(':');
+            Type = data[0];
+            Token = data[1];
+            Contents = data[2];
+        }
+
         public static Message FromString(string s) => new Message() { Type = "string", Token = GenerateToken(), Contents = s };
         public static Message FromInt(int i) => new Message() { Type = "int", Token = GenerateToken(), Contents = $"{i}" };
         public static Message FromFloat(float f) => new Message() { Type = "float", Token = GenerateToken(), Contents = $"{f}" };
 
-        public override string ToString() => $"({Type}){Token}:{Contents}";
+        public override string ToString() => $"{Type}:{Token}:{Contents}";
 
         static int tokenLength = 20;
         static Random rng = new Random();
         static string GenerateToken() {
             StringBuilder tokenBuilder = new StringBuilder();
             for (int i = 0; i < tokenLength; i++)
-                tokenBuilder.Append((char)rng.Next(65, 122));
+                tokenBuilder.Append((char)rng.Next('A', 'Z'));
             return tokenBuilder.ToString();
         }
     }
